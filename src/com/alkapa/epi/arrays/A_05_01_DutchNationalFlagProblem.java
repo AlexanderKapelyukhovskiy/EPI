97b5a7c9e195e52fa48e13c171e963c402a965cf
Alexander Kapelyukhovsky <akapelyukhovsky@ingenio.com>
2022-05-16 21:10:52 -0700

    The Dutch National Flag Problem

---
+++ src/com/alkapa/epi/arrays/A_05_01_DutchNationalFlagProblem.java
@@ -0,0 +1,77 @@
+package com.alkapa.epi.arrays;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class A_05_01_DutchNationalFlagProblem {
+
+    public enum Color {RED, WHITE, BLUE};
+
+    public static void dutchFlagPartition1(int pivotIndex, List<Color> list) {
+        Color pivot = list.get(pivotIndex);
+        int smaller = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).ordinal() < pivot.ordinal()){
+                Collections.swap(list, smaller++, i);
+            }
+        }
+        int larger = list.size() - 1;
+        for (int i = list.size()-1; i >= 0; i--) {
+            if (list.get(i).ordinal() > pivot.ordinal()){
+                Collections.swap(list, larger--, i);
+            }
+        }
+    }
+
+    public static void dutchFlagPartition(int pivotIndex, List<Color> list) {
+        Color pivot = list.get(pivotIndex);
+
+        int smaller = 0, equal = 0, larger = list.size();
+
+        while (equal < larger) {
+            if (list.get(equal).ordinal() < pivot.ordinal()) {
+                Collections.swap(list, smaller++, equal++);
+            } else if (list.get(equal).ordinal() == pivot.ordinal()) {
+                ++equal;
+            } else {
+                Collections.swap(list, equal, --larger);
+            }
+        }
+    }
+
+    public static void partition(int partitionIndex, List<Color> list) {
+        Color pivot = list.get(partitionIndex);
+
+        int smaller = 0, equal = 0, larger = list.size();
+
+        while (equal < larger) {
+            if (list.get(equal).ordinal() < pivot.ordinal()){
+                Collections.swap(list, smaller++, equal++);
+            } else if (list.get(equal).ordinal() == pivot.ordinal()) {
+                ++equal;
+            } else {
+                Collections.swap(list, equal, --larger);
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        List<Color> list = new ArrayList<>();
+        list.add(Color.BLUE);
+        list.add(Color.RED);
+        list.add(Color.WHITE);
+        list.add(Color.BLUE);
+        list.add(Color.RED);
+        list.add(Color.RED);
+        list.add(Color.RED);
+        list.add(Color.WHITE);
+
+        partition(2, list);
+    }
+
+}
