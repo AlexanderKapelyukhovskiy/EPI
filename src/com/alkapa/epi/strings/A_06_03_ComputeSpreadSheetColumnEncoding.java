cc8a198db041db7b20ebaa0d4482490e6859b008
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2022-05-25 21:48:25 -0700

    6.3 Compute Spread Sheet Column Encoding

---
+++ src/com/alkapa/epi/strings/A_06_03_ComputeSpreadSheetColumnEncoding.java
@@ -0,0 +1,15 @@
+package com.alkapa.epi.strings;
+
+public class A_06_03_ComputeSpreadSheetColumnEncoding {
+    public static int encode(String numAsString) {
+        return numAsString.chars().reduce(0, (x, c) -> x * 26 + c - 'A' + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(encode("A"));
+        System.out.println(encode("B"));
+        System.out.println(encode("AA"));
+        System.out.println(encode("AB"));
+    }
+}
+
