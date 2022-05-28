a057c9181863a75a5bfbd1932584630aa5aff9d1
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2022-05-27 17:12:04 -0700

    6.5 Test Palindromicity

---
+++ src/com/alkapa/epi/strings/A_06_05_TestPalindromicity.java
@@ -0,0 +1,32 @@
+package com.alkapa.epi.strings;
+
+public class A_06_05_TestPalindromicity {
+    public static boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!Character.isAlphabetic(s.charAt(i))) {
+                i++;
+                continue;
+            }
+            if (!Character.isAlphabetic(s.charAt(j))) {
+                j++;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void test(String s) {
+        boolean isPalindrome = isPalindrome(s);
+        System.out.println(String.format("%b - %s", isPalindrome, s));
+    }
+
+    public static void main(String[] args) {
+        test("aba");
+        test("abba");
+        test("abbat");
+    }
+}
