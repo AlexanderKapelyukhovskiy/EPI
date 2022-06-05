5c42d9bb0aa61cfdca694c8bba4132634ef87b90
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2022-06-04 23:32:59 -0700

    9.1: Test If Binary Tree Is Height Balanced

---
+++ src/com/alkapa/epi/trees/A_09_01_TestIfBinaryTreeIsHeightBalanced.java
@@ -0,0 +1,35 @@
+package com.alkapa.epi.trees;
+
+public class A_09_01_TestIfBinaryTreeIsHeightBalanced {
+    public static BalanceStatusWithWeight checkBalance(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return new BalanceStatusWithWeight(true, -1);
+        }
+
+        BalanceStatusWithWeight leftResult = checkBalance(root.left);
+        if (!leftResult.balanced) {
+            return leftResult;
+        }
+        BalanceStatusWithWeight rightResult = checkBalance(root.right);
+        if (!rightResult.balanced) {
+            return rightResult;
+        }
+
+        boolean isBalanced = Math.abs(leftResult.height - rightResult.height) <= 1;
+        int height = Math.max(leftResult.height, rightResult.height) + 1;
+        return new BalanceStatusWithWeight(isBalanced, height);
+    }
+    public static void test(BinaryTreeNode<Integer> root) {
+        BalanceStatusWithWeight res = checkBalance(root);
+        System.out.println(res.balanced);
+        System.out.println(res.height);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> notBalanced = BinaryTreeNode.createNotBalancedBinaryTree();
+        test(notBalanced);
+
+        BinaryTreeNode<Integer> balanced = BinaryTreeNode.createBalancedBinaryTree();
+        test(balanced);
+    }
+}
--- src/com/alkapa/epi/trees/BinaryTreeNode.java
+++ src/com/alkapa/epi/trees/BinaryTreeNode.java
@@ -12,4 +12,24 @@ public class BinaryTreeNode<T> {//extends TreeLike<T, BinaryTreeNode<T>> {}
     public BinaryTreeNode(T data) {
         this(data, null, null);
     }
+
+    public static BinaryTreeNode<Integer> createBalancedBinaryTree() {
+        return  new BinaryTreeNode<>(
+                1,
+                new BinaryTreeNode<>(2, new BinaryTreeNode<>(3), new BinaryTreeNode<>(4)),
+                new BinaryTreeNode<>(5, new BinaryTreeNode<>(6), new BinaryTreeNode<>(7))
+        );
+    }
+
+    public static BinaryTreeNode<Integer> createNotBalancedBinaryTree() {
+        return new BinaryTreeNode<>(
+                1,
+                new BinaryTreeNode<>(2, new BinaryTreeNode<>(3),
+                        new BinaryTreeNode<>(4,
+                                new BinaryTreeNode<>(5,
+                                        new BinaryTreeNode<>(6),
+                                        new BinaryTreeNode<>(7)),
+                                null)),
+                null);
+    }
 }
