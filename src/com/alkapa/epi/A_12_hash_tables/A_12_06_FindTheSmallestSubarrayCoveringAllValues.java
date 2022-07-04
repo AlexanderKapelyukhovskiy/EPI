11c8bdda78e3bd0b70dcd03eeffafabf8c02228b
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2022-07-04 14:40:32 -0700

    12.6: Find The Smallest Subarray Covering All Values (Fixed)

--- src/com/alkapa/epi/A_12_hash_tables/A_12_06_FindTheSmallestSubarrayCoveringAllValues.java
+++ src/com/alkapa/epi/A_12_hash_tables/A_12_06_FindTheSmallestSubarrayCoveringAllValues.java
@@ -1,6 +1,9 @@
 package com.alkapa.epi.A_12_hash_tables;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class A_12_06_FindTheSmallestSubarrayCoveringAllValues {
     private static class Subarray {
@@ -36,18 +39,98 @@ public class A_12_06_FindTheSmallestSubarrayCoveringAllValues {
         return result;
     }
 
+    public static Subarray findSmallestSubarrayCoveringSetV2(List<String> paragraph, Set<String> keywords) {
+        Map<String, Long> keywordsToCover = keywords.stream().collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        Subarray result = new Subarray(-1, -1);
+        int remainingToCover = keywords.size();
+        for(int left = 0, right = 0; right < paragraph.size(); ++ right) {
+
+            if (keywordsToCover.containsKey(paragraph.get(right))
+                    && keywordsToCover.put(paragraph.get(right), keywordsToCover.get(paragraph.get(right)) - 1) >= 1) {
+                --remainingToCover;
+            }
+
+            while(remainingToCover == 0) {
+                if ((result.start == -1 && result.end == -1)
+                    || right - left < result.end - result.start) {
+                    result.start = left;
+                    result.end = right;
+                }
+                if (keywordsToCover.containsKey(paragraph.get(left))
+                        && keywordsToCover.put(paragraph.get(left), keywordsToCover.get(left) + 1) >= 0)
+                {
+                    ++remainingToCover;
+                }
+                ++left;
+            }
+        }
+
+        return result;
+    }
+
+    private static Integer getValueFroFirstEntry(LinkedHashMap<String, Integer> m) {
+        Integer result = null;
+        for (Map.Entry<String, Integer> entry : m.entrySet()) {
+            result = entry.getValue();
+            break;
+        }
+        return result;
+    }
+
+    public static Subarray findSmallestSubarrayCoveringSetV3(Iterator<String> paragraph, Set<String> keywords) {
+        LinkedHashMap<String, Integer> dict = new LinkedHashMap<>(keywords.size(), 1, true);
+        for(String s : keywords) {
+            dict.put(s, null);
+        }
+        int numStringsFromQueryStringsSeenSoFar = 0;
+
+        Subarray result = new Subarray(-1, -1);
+        int idx = 0;
+        while (paragraph.hasNext()) {
+            String s  = paragraph.next();
+            if (dict.containsKey(s)) {
+                Integer it = dict.get(s);
+                if (it == null) {
+                    numStringsFromQueryStringsSeenSoFar++;
+                }
+                dict.put(s, idx);
+
+                if (numStringsFromQueryStringsSeenSoFar == keywords.size()) {
+                    if ((result.start == -1 && result.end == -1)
+                        || idx - getValueFroFirstEntry(dict) < result.end - result.start) {
+                        result.start = getValueFroFirstEntry(dict);
+                        result.end = idx;
+                    }
+                }
+            }
+            ++idx;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         List<String> paragraph = new ArrayList<>();
-        paragraph.add("1");
-        paragraph.add("2");
-        paragraph.add("3");
-        paragraph.add("4");
+        paragraph.add("apple");
+        paragraph.add("banana");
+        paragraph.add("apple");
+        paragraph.add("apple");
+        paragraph.add("dog");
+        paragraph.add("cat");
+        paragraph.add("apple");
+        paragraph.add("dog");
+        paragraph.add("banana");
+        paragraph.add("apple");
+        paragraph.add("cat");
+        paragraph.add("dog");
 
         Set<String> keywords = new HashSet<>();
-        keywords.add("2");
-        keywords.add("3");
+        keywords.add("banana");
+        keywords.add("cat");
 
-        Subarray res = findSmallestSubarrayCoveringSet(paragraph, keywords);
+        Subarray res = findSmallestSubarrayCoveringSetV3(paragraph.iterator(), keywords);
         System.out.println(String.format("%d %d", res.start, res.end));
     }
 }
