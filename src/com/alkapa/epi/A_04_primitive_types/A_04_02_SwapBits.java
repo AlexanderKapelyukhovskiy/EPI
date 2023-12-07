02ae609af8e2e12ca9e060c397b8f2108b4eae7e
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2023-12-06 21:21:12 -0800

    4.2: Swap bits (add description and comments) + formatting

--- src/com/alkapa/epi/A_04_primitive_types/A_04_02_SwapBits.java
+++ src/com/alkapa/epi/A_04_primitive_types/A_04_02_SwapBits.java
@@ -1,9 +1,39 @@
 package com.alkapa.epi.A_04_primitive_types;
 
+/**
+ * <h3>4.2 Swap bits</h3>
+ * <p>
+ * A 64-bit integer can be viewed as an array of 64 bits, with the bit at index 0 corresponding to the
+ * least significant bit (LSB), and the bit at index 63 corresponding to the most significant bit (MSB).
+ * Implement code that takes as input 64-bit integer and swaps the bits at indices i and j.
+ * </p>
+ * <p>i=1, j=6</p>
+ * <p>0 <b>1</b> 0 0 1 0 <b>0</b> 1</p>
+ * <p>0 <b>0</b> 0 0 1 0 <b>1</b> 1</p>
+ */
 public class A_04_02_SwapBits {
+    /**
+     * Swap bits in positions i in j of x
+     *
+     * @param x arrays of 64-bits
+     * @param i first position of bit to swap
+     * @param j second position of bit to swap
+     * @return array of 64-bits from x with swapped bites in positions i and j
+     */
     public static long swapBits(long x, int i, int j) {
-        if (((x >>> i) & 0x1) != ((x >>> j) & 0x1)) {
-            long mask = 0x1 << i | 0x1<<j;
+        // check bits in position i
+        boolean bitInI = ((x >>> i) & 0x1) == 0x1;
+        // check bits in position j
+        boolean bitInJ = ((x >>> j) & 0x1) == 0x1;
+        // swap only bits only if bits in i and j positions are different
+        if (bitInI != bitInJ) {
+            long bitInIMask = 0x1 << i;
+            long bitInJMask = 0x1 << j;
+
+            // create mask for bits in positions i and j
+            long mask = bitInIMask | bitInJMask;
+
+            // XOR x with mask to swap bits in positions i and j
             return x ^ mask;
         }
         return x;
@@ -19,8 +49,8 @@ public class A_04_02_SwapBits {
 
     public static void main(String[] args) {
         test("001", 2, 0, "100");
-        test("101", 2, 0, "101" );
-        test("0101", 3, 0, "1100" );
-        test("111010101010", 8, 1, "111110101000" );
+        test("101", 2, 0, "101");
+        test("0101", 3, 0, "1100");
+        test("111010101010", 8, 1, "111110101000");
     }
 }
