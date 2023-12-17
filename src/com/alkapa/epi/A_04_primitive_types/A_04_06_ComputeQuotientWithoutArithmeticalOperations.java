39ed6ad600e4da2ad6d5230ac1b8fbaa888ec5ab
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2023-12-17 15:24:33 -0800

    4.6: fix solution and add test with comments

--- src/com/alkapa/epi/A_04_primitive_types/A_04_06_ComputeQuotientWithoutArithmeticalOperations.java
+++ src/com/alkapa/epi/A_04_primitive_types/A_04_06_ComputeQuotientWithoutArithmeticalOperations.java
@@ -8,22 +8,43 @@ package com.alkapa.epi.A_04_primitive_types;
  * </p>
  */
 public class A_04_06_ComputeQuotientWithoutArithmeticalOperations {
+    /**
+     * devide without arithmetic operators
+     *
+     * @param x
+     * @param y
+     * @return quotient of x and y
+     */
     public static int divide(int x, int y) {
         int result = 0;
         int power = 32;
-        long yPower = (long) x << power;
+        long yPower = y << power;
         while (x >= y) {
+            // searching for biggest 2^k * y which is smaller than x
             while (yPower > x) {
+                // shift right 2^k * y for 1
                 yPower >>>= 1;
                 --power;
             }
+            // when we found k ( x >= 2^k * y ) we add 2^k to res and decrease x for 2^k * y
             result += 1 << power;
             x -= yPower;
         }
         return result;
     }
 
+    private static void test(int x, int y) {
+        int d = divide(x, y);
+        int expect = x / y;
+        if (d != expect) {
+            System.out.print("Error: ");
+        }
+        System.out.println(String.format("%d / %d = %d", x, y, d));
+    }
+
     public static void main(String[] args) {
-        System.out.println(divide(100, 90));
+        test(11, 2);
+        test(900, 90);
+        test(100, 90);
     }
 }
