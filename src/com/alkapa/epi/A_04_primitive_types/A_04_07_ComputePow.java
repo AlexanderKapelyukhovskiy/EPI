20ba14c55479c8f327150d9e0f81270d4cdc447a
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2023-12-23 17:23:08 -0800

    >4.7 Compute pow(x, y)

--- src/com/alkapa/epi/A_04_primitive_types/A_04_07_ComputePow.java
+++ src/com/alkapa/epi/A_04_primitive_types/A_04_07_ComputePow.java
@@ -1,24 +1,55 @@
 package com.alkapa.epi.A_04_primitive_types;
 
+/**
+ * <h3>4.7 Compute pow(x, y)</h3>
+ * <p>
+ * Write a program that takes a double x and an integer y and returns x^y. You can ignore overflow and
+ * underflow.
+ * </p>
+ */
 public class A_04_07_ComputePow {
+    /**
+     * Compute x power y
+     *
+     * @param x
+     * @param y
+     * @return pow(x, y)
+     */
     public static double power(double x, int y) {
         double result = 1;
         long power = y;
+
         if (y < 0) {
+            //if y is negative we should use 1/x to remove minus
             power *= -1;
             x = 1 / x;
         }
+
         while (power != 0) {
-            if ((power & 0x1) !=0 ){
+            if ((power & 0x1) != 0) {
+                // if has LBS as 1 we need to multiply by x and remove it
                 result *= x;
             }
+            // get x in pow 2
             x *= x;
+            // divide power by 2
             power >>>= 1;
         }
         return result;
     }
 
-    public static void main(String[] args){
-        System.out.println(power(3,3));
+    public static void test(double x, int y) {
+        double res = power(x, y);
+        double expected = Math.pow(x, y);
+        if (res != expected) {
+            System.out.print("Error");
+        }
+        System.out.println(String.format("%f ^ %d = %f, expected: %f", x, y, res, expected));
+    }
+
+    public static void main(String[] args) {
+        test(3, 3);
+        test(8, 5);
+        test(1.1, 11);
     }
 }
